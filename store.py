53385fee5375e45ce2bdd3ed676eb38f4d6ea372
Viktor Maier <viktor12-86@web.de>
2026-03-19 12:06:37 +0100

    Store integriert

---
+++ store.py
@@ -0,0 +1,43 @@
+from products import Product
+from typing import List
+
+class Store:
+    def __init__(self, products):
+        self.products = products
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def remove_product(self, product):
+        self.products.remove(product)
+
+    def get_total_quantity(self) -> int:
+        total = 0
+        for product in self.products:
+            total += product.quantity
+        return total
+
+    def get_all_products(self) -> List[Product]:
+        active_products = []
+        for product in self.products:
+            if product.is_active():
+                active_products.append(product)
+        return active_products
+
+    def order(self, shopping_list) -> float:
+        total_price = 0.0
+        for product, quantity in shopping_list:
+            total_price += product.buy(quantity)
+        return total_price
+
+
+
+product_list = [Product("MacBook Air M2", price=1450, quantity=100),
+                Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                Product("Google Pixel 7", price=500, quantity=250),
+               ]
+
+best_buy = Store(product_list)
+products = best_buy.get_all_products()
+print(best_buy.get_total_quantity())
+print(best_buy.order([(products[0], 1), (products[1], 2)]))
